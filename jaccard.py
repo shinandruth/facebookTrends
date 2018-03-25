9d7ba7fe280d3e0474b1aedba222202ece8ac28c
Shin Lee <lleeshin@gmail.com>
2018-03-25 17:52:28 -0500

    jaccard updated for change intervals

--- jaccard.py
+++ jaccard.py
@@ -6,10 +6,10 @@ import numpy as np
 from math import *
 
 #--------------CHANGE HERE ---------------------
-interval = 5
+interval = 45
 name = "1min_test"
-filename = "jaccard" + name + "-test"
-time_interval_filename = name + "_" + str(interval)
+filename = "jaccard_" + name
+time_interval_filename = filename + "_by" + str(interval) + "min"
 #--------------CHANGE END HERE-----------------
 
 def compute_jaccard(user1_vals, user2_vals):
@@ -18,21 +18,46 @@ def compute_jaccard(user1_vals, user2_vals):
     jaccard = len(intersection)/float(len(union))
     return jaccard
 
+
 def get_avg():
-    df = pd.read_csv(os.path.join("CSV//", name+".csv"))
-    total_run = df.scrapeid.max()
-    curr = 0
-    s = 0
+    df = pd.read_csv(os.path.join("CSV//", time_interval_filename+".csv"))
+    id = 0
+    t = 0
+    curr = []
     similarity = []
-    for n in range(int((total_run+1)/2)):
+    max_id = df.scrapeid.max()
+    while id < max_id:
         prev = curr
-        curr = (df.loc[df["scrapeid"] == n, "title"]).drop_duplicates().values.tolist()
+        curr = (df.loc[df["scrapeid"] == id, "title"]).drop_duplicates().values.tolist()
         if prev != 0:
             similarity.append(compute_jaccard(set(prev), set(curr)))
+        if id == 0:
+            id += interval - 1
+        else:
+            id += interval
     for val in similarity:
-        s += val
-    avg = s / len(similarity)
+        t += val
+    avg = t / len(similarity)
     return avg
+
+
+# def get_avg():
+#     df = pd.read_csv(os.path.join("CSV//", time_interval_filename+".csv"))
+#     total_run = df.scrapeid.max() / interval
+#     curr = 0
+#     s = 0
+#     similarity = []
+#     for n in range(int((total_run+1)/2)):
+#         prev = curr
+#         curr = (df.loc[df["scrapeid"] == n, "title"]).drop_duplicates().values.tolist()
+#         print("prev:", prev)
+#         print("curr:", curr)
+#         if prev != 0:
+#             similarity.append(compute_jaccard(set(prev), set(curr)))
+#     for val in similarity:
+#         s += val
+#     avg = s / len(similarity)
+#     return avg
 
 def create_input(avg):
     l = []
@@ -42,27 +67,26 @@ def create_input(avg):
     return l
 
 def plot():
-    df = pd.read_csv(os.path.join("CSV//", filename+".csv"))
+    df = pd.read_csv(os.path.join("CSV//", time_interval_filename+".csv"))
     ax = df.plot.bar(title="jaccard similarity", x='interval', y='avg', legend=True)
     fig = ax.get_figure()
     #plt.show()
-    fig.savefig("..//selenium_env//graphs//"+filename+".png")
-
+    fig.savefig("..//selenium_env//graphs//"+time_interval_filename+".png")
 
 def create_csv():
     output_data = []
     firstline = True
     scrapeid = 0
-    with open("CSV//"+name+".csv", "r", encoding="utf-8") as readfile,  open(filename+".csv", "a", encoding="utf-8",  newline="") as writefile:
+    with open("CSV//"+name+".csv", "r", encoding="utf-8") as readfile,  open("CSV//"+time_interval_filename+".csv", "a", encoding="utf-8",  newline="") as writefile:
         reader = csv.reader(readfile)
         writer = csv.writer(writefile)
+        writer.writerow(["type", "title", "description", "source", "uniquelink", "rank", "scrapeid", "timestamp"])
         #writer.writerow(["name", "interval", "average"])
         for row in reader:
             if firstline:
                 firstline = False
                 continue
             curr_scrapeid = row[6]
-            print("curr:", curr_scrapeid, "set:", scrapeid)
             if scrapeid == int(curr_scrapeid):
                 writer.writerow(row)
             elif int(curr_scrapeid) > scrapeid:
@@ -72,11 +96,12 @@ def create_csv():
                     scrapeid += interval
 
 if __name__ == "__main__":
-    #avg = get_avg()
-    #i = create_input(avg)
-    create_csv()
-    # with open(filename+".csv", "a", encoding="utf-8",  newline="") as f:
-    #     writer = csv.writer(f)
-    #     writer.writerow(["name", "interval", "avg"]) #Scrap ID
-    #     writer.writerow(i)
+    avg = get_avg()
+    i = create_input(avg)
+    #Create interval csv files
+    #create_csv()
+    with open("jaccard_CSV//"+filename+".csv", "a", encoding="utf-8",  newline="") as f:
+        writer = csv.writer(f)
+        #writer.writerow(["name", "interval", "avg"]) #Scrap ID
+        writer.writerow(i)
     #plot()
